8fbf4d1c77491cdd431bd9c05630ea6318d9a462
Juan Camilo Acevedo <jcacevedo@compuhora.com.co>
2020-11-12 21:12:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const { pool } = require("./dbConfig");
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false}));
+
+app.get("/users/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/users/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/users/dashboard", (req, res) => {
+    res.render("dashboard", {user: "conor"});
+});
+
+app.post('users/register', (req, res) => {
+    let { name, email, password, password2 } = req.body;
+
+    console.log({
+        name,
+        email,
+        password,
+        password2
+    });
+    let errors = [];
+
+    if (!name || !email || !password || password2) {
+        errors.push({message: "Please enter all fields"})
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
