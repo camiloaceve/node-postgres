075b5cf457e8db2324f13dbf52e660f3c236aa69
unknown <camilo.acevedo@evo-ion.com>
2020-11-16 14:07:35 -0500

    finalizing login implementation with node and desserts / test environment

--- app.js
+++ app.js
@@ -1,108 +1,150 @@
-const express = require('express');
-const app = express();
-const { pool } = require('./dbConfig');
-const bcrypt = require('bcrypt');
-const session = require('express-session');
-const flash = require('express-flash');
+const express = require('express')
+const app = express()
+const { pool } = require('./dbConfig')
+const bcrypt = require('bcrypt')
+const session = require('express-session')
+const flash = require('express-flash')
+const passport = require('passport')
 
-const PORT = process.env.PORT || 4000;
+const initializePassport = require('./passportConfig')
 
-app.get("/", (req, res) => {
-    res.render("index");
-});
+initializePassport(passport)
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false}));
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.use(
-    session({
-        secret: "secret",
+  session({
+    secret: 'secret',
 
-        resave: false,
+    resave: false,
 
-        saveUninitialized:false
-    })
-);
+    saveUninitialized: false
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(flash());
+app.use(flash())
 
-app.get("/users/register", (req, res) => {
-    res.render("register");
-});
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
 
-app.get("/users/login", (req, res) => {
-    res.render("login");
-});
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
 
-app.get("/users/dashboard", (req, res) => {
-    res.render("dashboard", {user: ""});
-});
+app.get('/users/dashboard', (req, res) => {
+  res.render('dashboard', { user: req.user.name })
+})
+
+app.get('/users/logout', (req, res) => {
+  req.logOut()
+  req.flash('success_msg', 'You have logged out')
+  res.redirect('/users/login')
+})
 
 app.post('/users/register', async (req, res) => {
-    let { name, email, password, password2 } = req.body;
+  let { name, email, password, password2 } = req.body
 
-    let errors = [];
+  let errors = []
 
-    console.log({
-        name,
-        email,
-        password,
-        password2
-    });
+  console.log({
+    name,
+    email,
+    password,
+    password2
+  })
 
-    if (!name || !email || !password || !password2) {
-        errors.push({message: "Please enter all fields"})
-    }
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: 'Please enter all fields' })
+  }
 
-    if (password.length < 8) {
-        errors.push({ message: "Password should be at least 8 characters"   });
-    }
+  if (password.length < 8) {
+    errors.push({ message: 'Password should be at least 8 characters' })
+  }
 
-    if (password !== password2) {
-        errors.push({ message: "Passwords do not match"});
-    }
+  if (password !== password2) {
+    errors.push({ message: 'Passwords do not match' })
+  }
 
-    if (errors.length > 0) {
-        res.render("register", { errors });
-    }else{
-        // Validation has passed
+  if (errors.length > 0) {
+    res.render('register', { errors })
+  } else {
+    // Validation has passed
 
-        let hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword);
+    let hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword)
 
-        pool.query(
-            `SELECT * FROM users WHERE email = $1`,
-            [email],
-            (err, results) => {
-                if (err) {
-                    console.log(err);
-                }
-                console.log(results.rows);
+    pool.query(
+      `SELECT * FROM users WHERE email = $1`,
 
-                if (results.rows.length > 0) {
-                    errors.push({ message: "Email already registered" });
-                    res.render("register", { errors })
-                } else {
-                    pool.query(
-                        `INSERT INTO users (name, email, password)
+      
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err)
+        }
+        if (results) {
+          console.log(results.rows)
+
+          if (results.rows.length > 0) {
+            errors.push({ message: 'Email already registered' })
+            res.render('register', { errors })
+          } else {
+            pool.query(
+              `INSERT INTO users (name, email, password)
                         VAlues ($1, $2, $3)
                         RETURNING id, password`,
-                        [name, email, hashedPassword],
-                        (err, results) => {
-                            if (err) {
-                                throw err;
-                            }
-                            console.log(results.rows);
-                            req.flash("success_msg", "You are now registered. Please log in");
-                            res.redirect("/users/login")
-                        }
-                    );
+              [name, email, hashedPassword],
+              (err, results) => {
+                if (err) {
+                  throw err
                 }
-            }
-        );
-    }
-});
+                console.log(results.rows)
+                req.flash(
+                  'success_msg',
+                  'You are now registered. Please log in'
+                )
+                res.redirect('/users/login')
+              }
+            )
+          }
+        }
+      }
+    )
+  }
+})
+
+app.post(
+  '/users/login',
+  passport.authenticate('local', {
+    successRedirect: 'users/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })
+)
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/users/dashboard')
+  }
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/users/login')
+}
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
